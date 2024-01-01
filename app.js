613f2565ee66636c94c4440e923dc7bf2aae4b33
1shyamp9 <1shyamp9@gmail.com>
2024-01-01 13:01:34 +0530

    second commit

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ import { configDotenv } from "dotenv";
 import express from "express";
 import cors from 'cors'
 import { userRoute } from "./Routes/userRoute.js";
-import { postRoute } from "./Routes/postRoute.js"; 
+import { postRoute } from "./Routes/postRoute.js";
 import cookieParser from "cookie-parser";
 
-configDotenv({path:"./Database/config.env"})
+configDotenv({ path: "./Database/config.env" })
 
-export const app = express();  
+export const app = express();
 app.use(express.json())
 app.use(cookieParser())
- 
+
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
 app.get('/', (req, res) => {
-    res.sendFile("Hmmm.. Its Running BaBy.😏"); 
+    res.status(200).json({
+        message: "Hmmm.. Its Running BaBy.😏"
+    })
 })
-app.use('/api/user',userRoute); 
-app.use('/api/post',postRoute);  
+
+app.use('/api/user', userRoute);
+app.use('/api/post', postRoute);  
